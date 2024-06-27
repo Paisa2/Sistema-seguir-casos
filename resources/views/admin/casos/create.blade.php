fb98728bbcf1cf61055acebaad5e153b60e81c20
Paisa2 <villca.fuente23@gmail.com>
2024-06-27 19:23:09 -0400

    modificacion del formulario de denuncias de casos

--- resources/views/admin/casos/create.blade.php
+++ resources/views/admin/casos/create.blade.php
@@ -2,201 +2,141 @@
 @extends('layouts.dashboard.index', ['activePage' => 'nueva solicitud', 'titlePage' => 'Nueva solicitud'])
 @section('main-content')
 
-<div class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <form action="#" method="POST">
-                    @csrf
-                    <div class="card">
-                        <div class="card-header card-header-primary">
-                            <h3> Registrar Caso de {{$unidad->nombre}}</h3>
-                            <p class="card-category">Ingresar datos Generales del caso Denuncia</p>
-                        </div>
-                            <div class="card-body">
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Numero de Caso</label>
-                                    <div class="col-sm-7">
-                                        <input type="text" class="form-control" name="" placeholder="Ingrese Nª de caso" value="" autofocus minlength="1" maxlength="3">
-                                    </div>
+            <div class="container shadow-lg p-3 mb-5 bg-body rounded">
+                <div class="card-header card-header-primary">
+                    <h3> Registrar Caso de {{$unidad->nombre}}</h3>
+                    <p class="card-category">Ingresar datos Generales del caso Denuncia</p>
+                    <!-- Divider -->
+                    <hr class="sidebar-divider my-0">
+                    <br>
+                        <form class="row g-3" action="#" method="POST">
+                            @csrf
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">Numero de Caso</label>
+                                    <input type="text" class="form-control" name="" placeholder="Ingrese Nª de caso" value="" autofocus minlength="1" maxlength="3">
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Tipologia del caso</label>
-                                    <div class="col-sm-7">
-                                        <input type="text" class="form-control" name="" placeholder="Ingrese la tipologia del caso" value="" autofocus minlength="1" maxlength="50">
-                                    </div>
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">Tipologia del caso</label>
+                                    <input type="text" class="form-control" name="" placeholder="Ingrese la tipologia" value="" autofocus minlength="1" maxlength="50">
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Responsable del Caso</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-4">
+                                    <label for="" class="form-label">Responsable Caso</label>
                                         <select name="" id="" class="form-control" value="" required>
-                                        <option value="">-- Selecciona el Responsable--</option>
-                                        <option value="TrabasjadorSocial" @if(old('') == 'TrabasjadorSocial') selected @endif>Trabajador Social</option>
-                                        <option value="Psicologo" @if(old('') == 'Psicologo') selected @endif>Psicologo</option>
-                                        <option value="Abogado" @if(old('') == 'Abogado') selected @endif>Abogado</option>
+                                            <option value="">-- Selecciona el Responsable--</option>
+                                            <option value="TrabasjadorSocial" @if(old('') == 'TrabasjadorSocial') selected @endif>Trabajador Social</option>
+                                            <option value="Psicologo" @if(old('') == 'Psicologo') selected @endif>Psicologo</option>
+                                            <option value="Abogado" @if(old('') == 'Abogado') selected @endif>Abogado</option>
                                         </select>
-                                    </div>
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Etapa del caso</label>
-                                    <div class="col-sm-7">
-                                        <input type="text" class="form-control" name="" placeholder="Ingrese la tipologia del caso" value="" autofocus minlength="1" maxlength="50">
-                                    </div>
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">Etapa del caso</label>
+                                        <input type="text" class="form-control" name="" placeholder="Ingrese la Etapa" value="" autofocus minlength="1" maxlength="50">
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Fecha de Registro</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">Fecha de Registro</label>
                                         <input name="dia" type="date" id="fechaReserva" class="form-control" placeholder="Dia Reserva" value="{{old('dia')}}" required>
-                                    </div>
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Derivar Unidades</label>
-                                    <div class="col-sm-7">
-                                        <select name="" id="" class="form-control" value="" required>
-                                        <option value="">-- Selecciona la Unidad--</option>
-                                        <option value="AdultoMayor" @if(old('') == 'AdultoMayor') selected @endif>Adulto-Mayor</option>
-                                        <option value="Slim" @if(old('') == 'Slin') selected @endif>Slim</option>
-                                        <option value="Defensoria" @if(old('') == 'Defensoria') selected @endif>Defensoria</option>
-                                        <option value="Discapacidad" @if(old('') == 'Discapacidad') selected @endif>Discapacidad</option>
-                                        </select>
-                                    </div>
+                                <div class="col-sm-4">
+                                    <label for="" class="form-label">Derivar Casos</label>
+                                    <input type="text" class="form-control" name="" placeholder="Ingrese la derivacion" value="" autofocus minlength="1" maxlength="50">
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Archivos</label>
-                                    <div class="col-sm-7">
-                                        <input type="text" class="form-control" name="" placeholder="Subir Archivos" value="" autofocus minlength="1" maxlength="50">
-                                    </div>
+                                <div class="col-sm-6">
+                                    <label for="" class="form-label">Archivos</label>
+                                        <input type="file" class="form-control" name="image" placeholder="Subir Archivos" value="" autofocus minlength="1" maxlength="50">
                                 </div>
-                                <br>
-                                <!-- Divider -->
-                                <hr class="sidebar-divider my-0">
+                            <br>
 
-                                <div class="card-header card-header-primary">
-                                    <p class="card-category">Ingresar datos Generales del caso Denunciante</p>
-                                </div>
-
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Nombre</label>
-                                    <div class="col-sm-7">
+                            <p class="card-category">Ingresar datos Generales del caso Denunciante</p>
+                            <hr class="sidebar-divider my-0">
+                            <br>
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">Nombre</label>
                                         <input type="text" class="form-control" name="" placeholder="Ingrese el nombre" value="" autofocus minlength="1" maxlength="50">
-                                    </div>
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Apellido</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">Apellido</label>
                                         <input type="text" class="form-control" name="" placeholder="Ingrese el apellido" value="" autofocus minlength="1" maxlength="50">
-                                    </div>
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">CI</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">CI</label>
                                         <input type="number" class="form-control" name="" placeholder="Ingrese el CI" value="" autofocus minlength="1" maxlength="7">
-                                    </div>
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Sexo</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">Sexo</label>
                                         <input type="text" class="form-control" name="" placeholder="Ingrese el sexo" value="" autofocus minlength="1" maxlength="1">
-                                    </div>
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Edad</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">Edad</label>
                                         <input type="number" class="form-control" name="" placeholder="Ingrese la edad" value="" autofocus minlength="1" maxlength="10">
-                                    </div>
                                 </div>
 
-
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Ocupacion</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-4">
+                                    <label for="" class="form-label">Ocupacion</label>
                                         <input type="text" class="form-control" name="" placeholder="Ingrese la ocupacion" value="" autofocus minlength="1" maxlength="50">
-                                    </div>
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Estado Civil</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-4">
+                                    <label for="" class="form-label">Estado Civil</label>
                                         <input type="text" class="form-control" name="" placeholder="Ingrese el E. Civil" value="" autofocus minlength="1" maxlength="50">
-                                    </div>
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Telefono</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-4">
+                                    <label for="" class="form-label">Telefono</label>
                                         <input type="number" class="form-control" name="" placeholder="Ingrese el telefono" value="" autofocus minlength="1" maxlength="8">
-                                    </div>
                                 </div>
-                                <br>
-                                <!-- Divider -->
-                                <hr class="sidebar-divider my-0">
-                                    <div class="card-header card-header-primary">
-                                        <p class="card-category">Ingresar datos Generales del caso Denunciado</p>
-                                    </div>
+                            <br>
+                            <p class="card-category">Ingresar datos Generales del caso Denunciado</p>
+                            <!-- Divider -->
+                            <hr class="sidebar-divider my-0">
 
-                                    <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Nombre</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">Nombre</label>
                                         <input type="text" class="form-control" name="" placeholder="Ingrese el nombre" value="" autofocus minlength="1" maxlength="50">
-                                    </div>
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Apellido</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">Apellido</label>
                                         <input type="text" class="form-control" name="" placeholder="Ingrese el apellido" value="" autofocus minlength="1" maxlength="50">
-                                    </div>
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">CI</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">CI</label>
                                         <input type="number" class="form-control" name="" placeholder="Ingrese el CI" value="" autofocus minlength="1" maxlength="7">
-                                    </div>
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Sexo</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">Sexo</label>
                                         <input type="text" class="form-control" name="" placeholder="Ingrese el sexo" value="" autofocus minlength="1" maxlength="1">
-                                    </div>
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Edad</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">Edad</label>
                                         <input type="number" class="form-control" name="" placeholder="Ingrese la edad" value="" autofocus minlength="1" maxlength="10">
-                                    </div>
                                 </div>
 
-                                <div class="row">
-                                    <label for="" class="col-sm-2 col-form-label">Telefono</label>
-                                    <div class="col-sm-7">
+                                <div class="col-sm-3">
+                                    <label for="" class="form-label">Telefono</label>
                                         <input type="number" class="form-control" name="" placeholder="Ingrese el telefono" value="" autofocus minlength="1" maxlength="8">
-                                    </div>
                                 </div>
 
-                                    <div class="col-md-offset-4 col-md-10 text-center mt-3">
-                                        <button type="submit" class="btn btn-primary">enviar</button>
-                                        <a href="{{ URL::previous() }}" class="btn btn-secondary">Cancelar</a>
-                                    </div>
-                        </div>
+                                <div class="col-md-offset-4 col-md-10 text-center mt-3">
+                                        <button type="submit" class="btn btn-primary">Registrar</button>
+                                        <a href="{{ URL::previous() }}" class="btn btn-danger">Cancelar</a>
+                                </div>
                     </div>
                 </form>
             </div>
-        </div>
-    </div>
-</div>
+
     <script>
             var fecha = new Date();
         var anio = fecha.getFullYear();
